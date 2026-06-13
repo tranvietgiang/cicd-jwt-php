0423dfe1808e857c1fe1b36afc620244666a99d3
tran viet giang <tranvietgiang25@gmail.com>
2026-06-14 02:00:23 +0700

    git action change 2

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE IF NOT EXISTS cicd_jwt_php
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
-USE cicd_jwt_php;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
